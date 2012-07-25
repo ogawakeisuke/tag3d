41b223bc31494ef0b0842cb3ac58518de1d32c23
nulltask <nulltask@gmail.com>
2012-07-25 19:37:57 +0900

    parse dom

--- test.js
+++ test.js
@@ -1,7 +1,27 @@
  $(document).ready(function(){
 //  var html_data = $('html').children();
   var html_data =  document.getElementsByTagName('html')[0].innerHTML;
-  alert(html_data);
+  // alert(html_data);
 
   return false;
+});
+
+var depth = 0;
+
+$(function() {
+  var body = document.body;
+  
+  function parse(el) {
+    console.log(depth,  el, getComputedStyle(el));
+    if (el.hasChildNodes()) {
+      for (var i = 0; i < el.children.length; i++) {
+        parse(el.children[i]); // recursive
+      }
+    } else {
+      ++depth;
+      return;
+    }
+  }
+  
+  parse(body);
 });
